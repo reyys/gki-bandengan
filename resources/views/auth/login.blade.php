9c8ed9201402416539d8e38b195c24a57a7a57a7
reyys <reyyy0207@gmail.com>
2024-06-11 16:49:42 +0700

    Landing Page Raw Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,22 @@
 <x-layout>
-    <form action="{{ route('login.store') }}" method="POST">
-        @csrf
-        <input name="email" type="email" placeholder="Input your email" />
-        @error('email')
-            <p>{{ $message }}</p>
-        @enderror
-        <input name="password" type="password" placeholder="Input your password" />
-        @error('password')
-            <p>{{ $message }}</p>
-        @enderror
-        <button type="submit">Login</button>
-    </form>
+    <div class="md:min-h-[90vh] flex items-center justify-center">
+        <form action="{{ route('login.store') }}" method="POST"
+            class="flex flex-col space-y-5 p-5 md:border w-full max-w-[500px]">
+            @csrf
+            <h3 class="text-center">Masuk Sebagai Jemaat</h3>
+            <label>Email</label>
+            <input class="p-3 border border-gray-300 rounded-md" name="email" type="email"
+                placeholder="Input your email" />
+            @error('email')
+                <p class="text-red-500">{{ $message }}</p>
+            @enderror
+            <label>Password</label>
+            <input class="p-3 border border-gray-300 rounded-md" name="password" type="password"
+                placeholder="Input your password" />
+            @error('password')
+                <p class="text-red-500">{{ $message }}</p>
+            @enderror
+            <button class="py-3 bg-primary text-white rounded-md" type="submit">Login</button>
+        </form>
+    </div>
 </x-layout>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,71 @@
 <x-layout>
-    <section class="container">
-        <div class="min-h-[80vh] flex items-center justify-center">
-            <a href="{{ route('schedules.index') }}">Lihat Jadwal Misa</a>
+    <section>
+        <div class="w-full relative h-fit">
+            <img class="w-full h-[92.5vh] object-cover" src="/images/church.jpg" />
+            <div class="absolute left-0 top-0 right-0 bottom-0 bg-black/40 z-[1]"></div>
+            <div class="absolute left-0 top-0 right-0 bottom-0 z-[2] flex flex-col items-center justify-center">
+                <h1 class="text-white text-center">Selamat datang di <br />Gereja Kristen Indonesia Bandengan</h1>
+                <a href="#schedules"
+                    class="bg-primary text-white p-3 rounded-md px-5 hover:scale-[0.95] transition-all mt-5 md:mt-8 text-xl">
+                    <button>Lihat Jadwal Misa</button>
+                </a>
+            </div>
+        </div>
+    </section>
+    <section id="schedules" class="container py-5 md:py-8 lg:py-12">
+        <div>
+            <div class="flex items-center justify-between">
+                <h2 class="font-medium">Jadwal Misa Terbaru</h2>
+                <a class="hover:underline" href="{{ route('schedules.index') }}">Lihat Semua</a>
+            </div>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5">
+                <div class="p-5 border bg-white rounded-md drop-shadow-lg">
+                    <h2>Hari Rabu Abu</h2>
+                    <p>Jl test, Bandengan, Jakarta Utara</p>
+                    <div class="w-full h-[1px] bg-gray-300 mt-5"></div>
+                    <div class="flex items-center space-x-5 py-5">
+                        <div class="flex flex-col items-center min-w-[60px] space-y-2">
+                            <h1 class="leading-none">12</h1>
+                            <p>JUN 24</p>
+                        </div>
+                        <div class="flex flex-col space-y-2">
+                            <h2 class="leading-none">Ibadah</h2>
+                            <h2 class="leading-none">Rabu Abu</h2>
+                        </div>
+                    </div>
+                    <div class="w-full h-[1px] bg-gray-300"></div>
+                    <div class="flex items-center space-x-5 py-5">
+                        <div class="flex flex-col items-center min-w-[60px] space-y-2">
+                            <h1 class="leading-none">11</h1>
+                            <p>WIB</p>
+                        </div>
+                        <div class="flex flex-col space-y-2">
+                            <h2 class="leading-none">Pdt. Aristo</h2>
+                            <p class="leading-none">Doa & Perjamuan Kudus</p>
+                        </div>
+                    </div>
+                    <div class="w-full h-[1px] bg-gray-300"></div>
+                    <p class="mt-5 italic text-gray-400">* Khusus Untuk Jemaat</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="w-full relative flex items-center justify-center h-[500px]">
+            <img class="absolute h-[500px] w-full left-0 top-0 right-0 bottom-0 object-cover"
+                src="/images/congregation.jpg" />
+            <div class="absolute left-0 top-0 right-0 bottom-0 bg-black/50 z-[1]"></div>
+            <div class="w-full flex flex-col justify-center items-center space-y-5 relative z-[2]">
+                <h2 class="text-white">Gabung Sebagai Jemaat</h2>
+                <p class="text-white max-w-[600px] leading-loose text-center">Bergabung bersama kami hari ini untuk
+                    mengabdi. Umur
+                    berapapun,
+                    Dimanapun dan
+                    Kapanpun</p>
+                <a href={{ route('register') }}>
+                    <button class="bg-primary px-5 py-3 text-white rounded-md">Daftar Sekarang</button>
+                </a>
+            </div>
         </div>
     </section>
 </x-layout>
