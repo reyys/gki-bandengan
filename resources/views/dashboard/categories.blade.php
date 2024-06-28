d7571c3693d1a9dbfe00d4cceea66ce716a099e6
reyys <reyyy0207@gmail.com>
2024-06-28 14:20:41 +0700

    Final adjustments

--- resources/views/dashboard/categories.blade.php
+++ resources/views/dashboard/categories.blade.php
@@ -1,9 +1,11 @@
 <x-layout.dashboard>
     <div class="flex flex-col space-y-5 md:space-y-0 md:flex-row items-center justify-between">
         <h3>Categories</h3>
-        <a href={{ route('categories.create') }} class="p-3 bg-primary text-white rounded-md px-5">
-            Buat Kategori Baru
-        </a>
+        @if (auth()->check() && auth()->user()->is_admin)
+            <a href={{ route('categories.create') }} class="p-3 bg-primary text-white rounded-md px-5">
+                Buat Kategori Baru
+            </a>
+        @endif
     </div>
     <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 md:gap-8 mt-5">
         @foreach ($categories as $category)
